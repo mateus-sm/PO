cb5633515a30b6a87c2a0f604102926ade42c388
Mateus Maciel <matteus6537@hotmail.com>
2025-08-14 10:57:52 -0300

    UPDATE

---
+++ src/main/java/org/example/listaDuplamenteEncadeada/Main.java
@@ -0,0 +1,36 @@
+package org.example.listaDuplamenteEncadeada;
+
+public class Main {
+    public static void main(String[] args) {
+        Lista l = new Lista();
+
+        l.inserirNoInicio(1);
+        l.inserirNoFinal(2);
+        //l.inserirNoFinal(3);
+        l.inserirNoFinal(4);
+        l.inserirNoFinal(5);
+        l.exibir();
+
+        int elem = 1;
+        System.out.printf("Elemento %d %s\n", elem, l.buscaExaustiva(elem) != null ? "Encontrado!" : "Não encontrado");
+
+        l.remover(elem);
+        System.out.println("Removendo " + elem);
+        l.exibir();
+
+        elem = 4;
+        l.remover(elem);
+        System.out.println("Removendo " + elem);
+        l.exibir();
+
+        elem = 5;
+        l.remover(elem);
+        System.out.println("Removendo " + elem);
+        l.exibir();
+
+        elem = 2;
+        l.remover(elem);
+        System.out.println("Removendo " + elem);
+        l.exibir();
+    }
+}
