fdee51dbf271e09361685a8f9a208da2b851a425
Mateus Maciel <matteus6537@hotmail.com>
2025-08-22 12:07:43 -0300

    UPDATE

--- src/main/java/org/example/primeiroBim/ordenacao/insercaoBinaria/BinarySort.java
+++ src/main/java/org/example/primeiroBim/ordenacao/insercaoBinaria/BinarySort.java
@@ -11,6 +11,39 @@ public class BinarySort {
     }
 
     public static void insercaoBinaria(int[] vet) {
+        int i = 0, pos, aux;
 
+        while (i < vet.length - 1) {
+            i++;
+            aux = vet[i];
+            pos = buscaBinaria(vet, i);
+
+            while (i > pos) {
+                vet[i] = vet[i - 1];
+                i--;
+            }
+
+            vet[pos] = aux;
+        }
+    }
+
+    public static int buscaBinaria(int[] vet, int TL) {
+        int meio, inicio = 0, fim = TL - 1, aux = vet[TL];
+        meio = (inicio + fim) / 2;
+
+        while (inicio < fim) {
+            if (vet[meio] > aux) {
+                fim = meio - 1;
+            } else {
+                inicio = meio + 1;
+            }
+
+            meio = (inicio + fim) / 2;
+        }
+
+        if (vet[TL] > vet[TL - 1]) {
+            return TL;
+        }
+        return meio;
     }
 }
