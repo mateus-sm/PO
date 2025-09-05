eed53e65a9f9e2beb229b91fa80cba1b0290149f
Mateus Maciel <matteus6537@hotmail.com>
2025-09-05 08:19:34 -0300

    Teste

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello World!");
+        System.out.printf("Hello World!");
     }
 }
